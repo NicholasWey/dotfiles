c85209684e14d7ee04ea604737ac48e8570b80c5
NickWey111 <niwe7487@colorado.edu>
2026-03-28 13:34:24 -0600

    feat: add neogit interactive git UI

---
+++ lua/plugins/git.lua
@@ -0,0 +1,23 @@
+-- lua/plugins/git.lua
+return {
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'sindrets/diffview.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
+    cmd = 'Neogit',
+    keys = {
+      { '<leader>gg', '<cmd>Neogit<cr>', desc = 'Open Neo[g]it' },
+    },
+    config = function()
+      require('neogit').setup {
+        integrations = {
+          diffview = true,
+          telescope = true,
+        },
+      }
+    end,
+  },
+}
