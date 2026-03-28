6a92fcf287ece0cc7f10873df736714f1a54ba63
NickWey111 <niwe7487@colorado.edu>
2026-03-28 14:12:52 -0600

    fix: use catppuccin-mocha as explicit lualine theme name

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -28,7 +28,7 @@ return {
     config = function()
       require('lualine').setup {
         options = {
-          theme = 'catppuccin',
+          theme = 'catppuccin-mocha',
           globalstatus = true,
           component_separators = { left = '', right = '' },
           section_separators = { left = '', right = '' },
